ee9c7bf4a11d00b1feaed3de6c7cc2cc9b7a148f
thefogui <vitorthefogui@gmail.com>
2018-02-20 20:32:09 +0100

    Update figura.h

--- figura.h
+++ figura.h
@@ -1,10 +1,15 @@
 #ifndef FIGURA_H
 #define FIGURA_H
 
+//un metodo virtual indica clase abstracta el hijo lo puede cargalo
+//protected para padre y hijo private solol para la clase que lo contiene
+
 class Figura{
-    virtual glm::vec4 obteNormal() = 0;
+
 public:
     Figura();
+    virtual ~Figura();
+    virtual glm::vec4 obteNormal() = 0;
 
 };
 
